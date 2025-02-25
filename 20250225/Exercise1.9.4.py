d46a5eba55a4781afda0d029eae6175f41697929
Ching-Yueh Cheng <ua140ua140ua140@gmail.com>
2025-02-25 20:24:16 +0800

    Create Exercise1.9.4.py

---
+++ 20250225/Exercise1.9.4.py
@@ -0,0 +1,11 @@
+print(type(765))
+print(type(2.718))
+print(type('2 pi'))
+print(type(abs(-7)))
+print(type(abs(-7.0)))
+print(type(abs))
+print(type(int))
+print(type(type))
+#abs取絕對值
+#int整數.浮點數
+#type判斷形式
