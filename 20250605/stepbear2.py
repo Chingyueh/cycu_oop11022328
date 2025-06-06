98d32477c09057a503fbcc329a41b5d5f6924a41
Ching-Yueh Cheng <ua140ua140ua140@gmail.com>
2025-06-06 21:47:32 +0800

    Update stepbear2.py

--- 20250605/stepbear2.py
+++ 20250605/stepbear2.py
@@ -242,7 +242,7 @@ import json
 import os
 
 client = openai.OpenAI(
-    api_key="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",
+    api_key="請找我拿API金鑰",
     base_url="https://api.groq.com/openai/v1"
 )
 
